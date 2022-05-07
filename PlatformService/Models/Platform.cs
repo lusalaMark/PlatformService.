29c5a4169348f3a5df9faac4febb567172f45daa
lusalamark <lusalamark63@gmail.com>
2022-05-07 23:11:55 +0300

    Added Models For the PlatformMicroservice

---
+++ PlatformService/Models/Platform.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PlatformService.Models
+{
+    public class Platform
+    {
+        [Key]
+        [Required]
+        public Guid Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Publisher { get; set; }
+        [Required]
+        public string Cost { get; set; }
+
+    }
+}

3a188745e12c29cfa7d4f7c3ddcbd5359b6718fa
lusalamark <lusalamark63@gmail.com>
2022-05-07 23:24:02 +0300

    Added DbContext

---
+++ PlatformService/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> optins) : base (optins)  
+        {
+
+        }
+
+        public DbSet<Platform> Platforms { get; set; }
+    }
+}
